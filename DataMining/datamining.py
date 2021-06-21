f85683ad9c2b7cde481ea5a0631973181bee14c6
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-21 08:28:17 -0400

    Corrected regex for speed
    I do not know why but be replacing the space by his regex symbol \s then it works with the article (document)
    
    The only test (teststr4) that failed for 4.yaml is to find the data inside a table because the table convert to text is quite messy.

--- DataMining/datamining.py
+++ DataMining/datamining.py
@@ -9,15 +9,16 @@ document = BeautifulSoup(f.read(),features='html.parser').get_text()
 f.close()
 
 # Get the RPM range
+speed_rex = r'(\d+)?,?\s?(\d+)?,?\s?(\d+)?\s?\w+\s(\d+)\srpm'
 teststr1 = 'rotational speed between 650, 1700 and 2800 rpm'
 teststr2 = 'rotational speed greater than 1700 rpm cause the formation'
 teststr3 = 'synthesized PLA (at 650 rpm) decreased'
 teststr4 = 'Table 3. The average fiber diameters of non-woven PLA fiber (L130).Rotational speed (rpm)Average fiber diameter (μm)190 °C210 °C230 °C65018.6 ± 5.311.8 ± 3.511.2 ± 4.7170015.4 ± 4.513.3 ± 5.711.4 ± 4.7280014.1 ± 5.49.6 ± 4.15.5 ± 2.7PLA, polylactic acid.'
-speed1 = re.findall(r'(\d+)?,? ?(\d+)?,? ?(\d+)? ?\w+ (\d+) rpm', teststr1)
-speed2 = re.findall(r'(\d+)?,? ?(\d+)?,? ?(\d+)? ?\w+ (\d+) rpm', teststr2)
-speed3 = re.findall(r'(\d+)?,? ?(\d+)?,? ?(\d+)? ?\w+ (\d+) rpm', teststr3)
-speed4 = re.findall(r'(\d+)?,? ?(\d+)?,? ?(\d+)? ?\w+ (\d+) rpm', teststr4)
-speed = re.findall(r'(\d+)?,? ?(\d+)?,? ?(\d+)? ?\w+ (\d+) rpm', document)
+speed1 = re.findall(speed_rex, teststr1)
+speed2 = re.findall(speed_rex, teststr2)
+speed3 = re.findall(speed_rex, teststr3)
+speed4 = re.findall(speed_rex, teststr4)
+speed = re.findall(speed_rex, document)
 
 
 
