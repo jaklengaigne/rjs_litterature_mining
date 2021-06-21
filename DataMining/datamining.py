7c7422b438ae1824791d3d51f29b327a4ff6a37e
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-21 16:30:08 -0400

    Iterated in all saved html file to check if we properly find the speed range
    https://www.codegrepper.com/code-examples/python/how+to+loop+through+list+of+files+in+a+folder+in+python
    
    I have changed the regex because I have found many variants in the writing of the spinneret speed as per rpm, RPM, r min-1. Also, I saw that I did get some other rpm that wasn't the one of the spinneret as the feeder/extruder so I put a minimum of an order of two for it. Even though I still got a mistake because I do get a collector rpm as data for my spinneret but I think I will live with it (005.html : "The speed of the spinning reservoir ranged from 15 000 to 65 000 RPM, while collector speed was 350 RPM.").
    
    To add, I got some deviation because articles often mention the range that the spinneret can go but scientists use smaller range to do their experiment. I will also live with that.
    
    In the cleaning process, I add a step to remove space and commas because some articles write 10 000 or 10,000 or 10000.
    
    I did not take in charge the scenario that somebody wrote a range like this 2000-4000. Usually I'm lucky and they mention again specific rpm for some test or result.
    
    I put a min speed to begin with that I kind of know we will obtain a smaller one (min0 = 300000000). Same for the max value.
    
    I use if isinstance while looping for min and max because by removing empty groups in each regex match. I sometimes get only one integer and we can't iterate if there is only one.
    
    Sometime the datamining does not find anything as in 013.html (not about RJS so it is not surprising that it did not mention rpm but in another view it could have been bad if we did get rpm that it is of no concern about our studied process..... to be thought) [it not speed:]
    
    I named the html file with three numbers because it gave the proper sorting for getting the test as per the SortedArcticlesByScores. Note that in github it also works like that but indexing can't start with a 0 exempt for zero.
    ----------------------------------------------------------------------------------------------
    REMARK :
    60% of success for the speed range as per manually mined data (.yaml) according to a sample of 5 precisely chosen articles.
    3 deviations are somewhat not too bad I think. They are often du to the authors mentioning the specification of the machine while using the full range of it for their experiments
    1 complete mistake : speed of collector and not of spinneret.

--- DataMining/datamining.py
+++ DataMining/datamining.py
@@ -1,44 +1,60 @@
+import os
 from bs4 import BeautifulSoup
 import codecs
 import re
 import pandas as pd
+import numpy as np
 
-# Get article text from a saved html file
-path = './ArticlesHtml/4.html'
-f = codecs.open(path, 'r', 'utf-8')
-document = BeautifulSoup(f.read(),features='html.parser').get_text()
-f.close()
-
-# Get RPM
-speed_rex = r'(\d+)?,?\s?(\d+)?,?\s?(\d+)?\s?\w+\s(\d+)\srpm'
-speed = re.findall(speed_rex, document)
-
-# Keep only a range
-# clean tuple list of speed (remove empty string → false min)
-speed = [(tuple(int(x) if x.isdigit() else x for x in _ if x)) for _ in speed]
-# find minimum speed
-min0 = 300000
-for match in range(len(speed)):
-    if isinstance(speed[match], int):
-        min_smatch = speed[match]
-    else:
-        min_smatch = min(speed[match])
-    if min_smatch < min0:
-        min0 = min_smatch
-        min_speed = min_smatch
-# find maximum speed        
-max0 = 0
-for match in range(len(speed)):
-    if isinstance(speed[match], int):
-        max_smatch = speed[match]
-    else:
-        max_smatch = max(speed[match])
-    if  max0 < max_smatch:
-        max0 = max_smatch
-        max_speed = max_smatch
-# put min max speed for a single article in a dataframe
-speed_range1 = [min_speed, max_speed]
-speed_range1 = pd.DataFrame(speed_range1, index=('min', 'max')).transpose()
-# put min max speed for each article in a dataframe
+# Iterated throught a bunch of local html file
+directory = './ArticlesHtml'
 speed_range = pd.DataFrame(columns=('min', 'max'))
-speed_range = speed_range.append(speed_range1)
+for filename in os.listdir(directory):
+    if filename.endswith('.html'):
+        # Get article text from a saved html file
+        path = os.path.join(directory, filename)
+        f = codecs.open(path, 'r', 'utf-8')
+        document = BeautifulSoup(f.read(),features='html.parser').get_text()
+        f.close()
+        
+        # Get RPM
+        speed_rex = r'(\d+?[\s|,]?\d{2,})?,?\s?(\d+?[\s|,]?\d{2,})?\s?\w+\s(\d+?[\s|,]?\d{2,})\s[r|R][p|P]?\s?[m|M]'
+        speed = re.findall(speed_rex, document)
+        
+        # Keep only a range
+        # clean tuple list of speed 
+        # (remove empty string → false min, space and comma)
+        speed = [(x.replace(',', '').replace(' ', '') if isinstance(x, str) else x for x in _ if x) for _ in speed]
+        speed = [(tuple(int(x) if x.isdigit() else x for x in _ if x)) for _ in speed]
+        # find minimum speed
+        min0 = 300000000
+        for match in range(len(speed)):
+            if isinstance(speed[match], int):
+                min_smatch = speed[match]
+            else:
+                min_smatch = min(speed[match])
+            if min_smatch < min0:
+                min0 = min_smatch
+                min_speed = min_smatch
+        # find maximum speed        
+        max0 = 0
+        for match in range(len(speed)):
+            if isinstance(speed[match], int):
+                max_smatch = speed[match]
+            else:
+                max_smatch = max(speed[match])
+            if  max0 < max_smatch:
+                max0 = max_smatch
+                max_speed = max_smatch
+        # If no match is found
+        if not speed:
+            min_speed = np.nan
+            max_speed = np.nan
+            min_smatch = None
+            max_smatch = None
+        # put min max speed for a single article in a dataframe
+        speed_range1 = [min_speed, max_speed]
+        speed_range1 = pd.DataFrame(speed_range1, index=('min', 'max')).transpose()
+        # put min max speed for each article in a dataframe
+        speed_range = speed_range.append(speed_range1)
+        # clean before the new iteration of the loop
+        del min_smatch, min_speed, max_smatch, max_speed
