0e65723e2ff1b5cd3b847b70987d90a40ef82f32
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-21 10:39:25 -0400

    Put min max speed in a dataframe using only 4.html
    ref to how I clean all matches to keep only founded group :
    https://stackoverflow.com/questions/21391763/remove-empty-strings-from-tuples-inside-a-list

--- DataMining/datamining.py
+++ DataMining/datamining.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import codecs
 import re
+import pandas as pd
 
 # Get article text from a saved html file
 path = './ArticlesHtml/4.html'
@@ -8,17 +9,36 @@ f = codecs.open(path, 'r', 'utf-8')
 document = BeautifulSoup(f.read(),features='html.parser').get_text()
 f.close()
 
-# Get the RPM range
+# Get RPM
 speed_rex = r'(\d+)?,?\s?(\d+)?,?\s?(\d+)?\s?\w+\s(\d+)\srpm'
-teststr1 = 'rotational speed between 650, 1700 and 2800 rpm'
-teststr2 = 'rotational speed greater than 1700 rpm cause the formation'
-teststr3 = 'synthesized PLA (at 650 rpm) decreased'
-teststr4 = 'Table 3. The average fiber diameters of non-woven PLA fiber (L130).Rotational speed (rpm)Average fiber diameter (μm)190 °C210 °C230 °C65018.6 ± 5.311.8 ± 3.511.2 ± 4.7170015.4 ± 4.513.3 ± 5.711.4 ± 4.7280014.1 ± 5.49.6 ± 4.15.5 ± 2.7PLA, polylactic acid.'
-speed1 = re.findall(speed_rex, teststr1)
-speed2 = re.findall(speed_rex, teststr2)
-speed3 = re.findall(speed_rex, teststr3)
-speed4 = re.findall(speed_rex, teststr4)
 speed = re.findall(speed_rex, document)
 
-
-
+# Keep only a range
+# clean tuple list of speed (remove empty string → false min)
+speed = [(tuple(int(x) if x.isdigit() else x for x in _ if x)) for _ in speed]
+# find minimum speed
+min0 = 300000
+for match in range(len(speed)):
+    if isinstance(speed[match], int):
+        min_smatch = speed[match]
+    else:
+        min_smatch = min(speed[match])
+    if min_smatch < min0:
+        min0 = min_smatch
+        min_speed = min_smatch
+# find maximum speed        
+max0 = 0
+for match in range(len(speed)):
+    if isinstance(speed[match], int):
+        max_smatch = speed[match]
+    else:
+        max_smatch = max(speed[match])
+    if  max0 < max_smatch:
+        max0 = max_smatch
+        max_speed = max_smatch
+# put min max speed for a single article in a dataframe
+speed_range1 = [min_speed, max_speed]
+speed_range1 = pd.DataFrame(speed_range1, index=('min', 'max')).transpose()
+# put min max speed for each article in a dataframe
+speed_range = pd.DataFrame(columns=('min', 'max'))
+speed_range = speed_range.append(speed_range1)
