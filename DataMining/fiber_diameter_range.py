0b7e96f32a78f3264b1de6acfd3b5543104d394e
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-24 16:10:47 -0400

    Regex change for 024.html (fiber diameter range)
    Non-breaking space (espace insécable)
    dia ... fiber (subject reversed + words between)
    about ≈ ...
    
    When there is only one number (not a range), it is better not to put the units as zero or one because it creates an error by continuing to another number not related.
    
    test1,  :+1:
    test2, :+1:  do not get random fiber diameter
    test3,  :+1:  do not get another dimension
    test4,  :+1:  do not get another dimension
    test5,  :-1:  do not get table's values
    test6,  :-1:  do not get paranthesis's
    test7,  :+1:
    test8,  :+1:
    
    Compared to the manually data mined for this article, the min and max are found even with the tests that didn't work.

--- DataMining/fiber_diameter_range.py
+++ DataMining/fiber_diameter_range.py
@@ -14,22 +14,22 @@ import numpy as np
 # =============================================================================
 dia_range = pd.DataFrame(columns=('min', 'max'))
 # Get article text from a saved html file
-path = './ArticlesHtml/005.html'
+path = './ArticlesHtml/024.html'
 f = codecs.open(path, 'r', 'utf-8')
 document = BeautifulSoup(f.read(),features='html.parser').get_text()
 f.close()
 
 # Get diameter
-dia_rex = r'fibers?\sdiameters?.*\s(\d+?\.?\d+\s?[μ|n]?m?\s\w+\s\d+?\.?\d+\s?[μ|n]?m?)|fibers?\sdiameters?.*[\s(](\d+[–|-]\d+\s?[μ|n]m)'
+dia_rex = r'n?a?n?o?fibers?\swith\san?\s?a?v?e?r?a?g?e?\sdiameters?\s.*[\s≈~](\d+?\.?\d+[\s| ]?[μ|n]m)|diameters?\sof\sn?a?n?o?fibers?\s.*\s(\d+?\.?\d+[\s| ]?[μ|n]m)|fibers?\sdiameters?.*\s(\d+?\.?\d+\s?[μ|n]?m?\s\w+\s\d+?\.?\d+\s?[μ|n]?m?)|fibers?\sdiameters?.*[\s(](\d+[–|-]\d+\s?[μ|n]m)'
 
-test1 = '9-11 recent efforts have focused on developing techniques for producing nanofibers with diameters less than 1 μm'
-test2 = 'iRJS control over the morphology, diameter, and alignment of sheets. a) Traditional nanofiber spinning systems relying on volatile solvents cause beading as described by the Raleigh–Plateau instability and revealed in SEM images of nylon (left scale bar = 20 μm, right scale bar = 5 μm)'
-test3 = 'For the case of nylon fibers, average fiber diameter decreased with increasing air gap distance (2 cm < d < 6 cm) and extruder rotation speed (15 kRPM < ω < 45 kRPM). In contrast, fiber diameter increased with increasing weight per volume solution concentration (5% w/v < C < 20% w/v). Within this parameter space, average nylon fiber diameters of 250 nm to 2.75 μm were produced (Figure 2c–e, Figure S2, Supporting Information).'
-test4 = 'Commercial PPTA fiber diameters are on the order of ≈10 μm and possess an inhomogeneous core-skin morphology that depends on proprietary production processes.'
-test5 = 'increasing spinning speed from 45 k RPM to 65k RPM decreased nanofiber diameter from 1300 to 800 nm (Figure 4a).'
-test6 = 'Spun at 65k RPM, PPTA concentrations of 1, 3, 5, or 10% (wt/v%) produced sheets of nanofibers with mean diameters of ≈500, 800, 850, or 900 nm, respectively (Figure 4a–d).'
-test7 = 'While commercial fiber diameters typically range from 10 to 20 μm,56 the significantly smaller diameter of the iRJS PPTA nanofibers (500–1000 nm) provides a 10–20 times increase in surface area-to-volume ratio.'
-test8 = 'fiber diameters ranged from 750 to 900 nm, density was .43 gm cm−3, and OOP values were .95 or greater. Mechanical testing of alginate–gelatin nanofibers after transglutaminase crosslinking (Modernist Pantry, Portsmouth, NH) crosslinking was obtained with a biaxial tension test (CellScale BioTester, Waterloo, Canada). For alginate nanofibers tested mechanically, fiber diameters ranged from 600 to 800 nm'
+test1 = 'polypropylene (PP) nanofibers with an average diameter of 790 nm are successfully prepared'
+test2 = 'Fang et al.11 prepared tin fluorophosphate glass fibers with average diameters ranging from 2 to 4 μm via centrifugal forcespinning.'
+test3 = 'the diameter of the centrifugal differential disk is 200 mm, and'
+test4 = 'the fibers were twisted into a cylinder about 3 mm in diameter and 20 mm in length, clamped on the nanotensile testing system (Aglient T150 UTM), the load value at start was 750 μN and'
+test5 = 'Table 2.The Influence of Four Studied Factors on the Average Diameter of Fibers Experimental number A B C D Average diameter (μm) 1 1 1 1 1 6.91'
+test6 = 'This is because the average diameter of fibers in Group 7 (0.79 μm) is much smaller than that in Group 2 (7.44 μm)'
+test7 = 'The maximum average diameter of fibers was 8.09 μm, '
+test8 = 'diameter of nanofibers was 160 nm for Group 7, as it is shown in Figure 7. It could indicate that the MDCE could fabricate the nanosized ultrafine fibers with a diameter of ~100 nm'
 dia = re.findall(dia_rex, document)
 dia1 = re.findall(dia_rex, test1)
 dia2 = re.findall(dia_rex, test2)
