205f23965866f1771eefe8a77dab8eb68ff71c85
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-27 11:01:13 -0400

    Trying to clean fiber range data - unsuccessfull for now
    -----------------------------------------------------------------
    To be clean and convert to the same SI units :
    temp. code
    alle = pd.DataFrame()
    alle = alle.append(dia)
                    0       1        2                  3        4
    1                                                      5–22 μm
    0                                   250 nm to 2.75 μm
    1                                      1300 to 800 nm
    2                                         10 to 20 μm
    3                                       600 to 800 nm
    0    400 ± 100 nm
    0  2389 ± 1313 nm
    1                                       0.2 to 2.0 μm
    0                  790 nm
    1                           790 nm
    2                          8.09 μm
    3                           160 nm
    4                  100 nm
    5                  160 nm
    -----------------------------------------------------------------
    With the same cleaning as speed range :
                   0
    1        5–22 μm
    0  250nmto2.75μm
    1    1300to800nm
    2       10to20μm
    3     600to800nm
    0   400 ± 100 nm
    0    2389±1313nm
    1     0.2to2.0μm
    0         790 nm
    1         790 nm
    2        8.09 μm
    3         160 nm
    4         100 nm
    5         160 nm
    See there is unbreakable space (3 types of spaces)
    -----------------------------------------------------------------
    I tried to isolate ± with re.search to do if x±y then [x+y, x-y] but units gave me problems with manipulation. When I remove all space in the beginning, I do not have anything to separate units from the digit. So I tried to do it in the beginning before removing spaces but I didn't succeed. Also, if I start processing each special case one by one then I get list AND str within the datatobeclean. Good puzzle!
    
    I also tried to split everything to get only digits and units all separated. Its created list of list and it's harder to work with.  I still think it is the best option to isolate units.
    
    I will try to convert units by doing something like
    for sublist in list
    for ele in sublist
    if nextele in sublist is str
       if µm do conversion on ele
       if nm do conversion on ele
    
    other loop
    if ele is str remove it

--- DataMining/fiber_diameter_range.py
+++ DataMining/fiber_diameter_range.py
@@ -5,64 +5,70 @@ import re
 import pandas as pd
 import numpy as np
 
-# =============================================================================
-# # Iterated throught a bunch of local html file
-# directory = './ArticlesHtml'
-# dia_range = pd.DataFrame(columns=('min', 'max'))
-# for filename in os.listdir(directory):
-#     if filename.endswith('.html'):
-# =============================================================================
+# Iterated throught a bunch of local html file
+directory = './ArticlesHtml'
 dia_range = pd.DataFrame(columns=('min', 'max'))
-# Get article text from a saved html file
-path = './ArticlesHtml/012.html'
-f = codecs.open(path, 'r', 'utf-8')
-document = BeautifulSoup(f.read(),features='html.parser').get_text()
-f.close()
+test = pd.DataFrame()
+for filename in os.listdir(directory):
+    if filename.endswith('.html'):
+        dia_range = pd.DataFrame(columns=('min', 'max'))
+        # Get article text from a saved html file
+        path = os.path.join(directory, filename)
+        f = codecs.open(path, 'r', 'utf-8')
+        document = BeautifulSoup(f.read(),features='html.parser').get_text()
+        f.close()
+        
+        # Get diameter
+        dia_rex = r'fib.*\s(\d+\s?±\s?\d+\s?[μ|n]m)|n?a?n?o?fibers?\swith\san?\s?a?v?e?r?a?g?e?\sdiameters?\s.*[\s≈~](\d+?\.?\d+[\s| ]?[μ|n]m)|diameters?\sof\sn?a?n?o?fibers?\s.*\s(\d+?\.?\d+[\s| ]?[μ|n]m)|fibers?\sdiameters?.*\s(\d+?\.?\d+\s?[μ|n]?m?\s\w+\s\d+?\.?\d+\s?[μ|n]m)|fibers?\sdiameters?.*[\s(](\d+[–|-]\d+\s?[μ|n]m)'
+        dia = re.findall(dia_rex, document)
+        
+        # Keep only a range
+        # clean tuple list of dia 
+        # (remove empty string → false min, space, comma and untis → conversion)
+        dia = [(re.split(' and | to | ± |–|-| | | ', x) if isinstance(x, str) else x for x in _ if x) for _ in dia]
+        
+#        dia = [(re.split('[\s| ]±[\s| ]', x) if re.search('±', x) else x for x in _ if x) for _ in dia]
+#        dia = [re.split('[\s| ]', x[1]) if isinstance(x, list) else x for x in dia if x]
 
-# Get diameter
-dia_rex = r'fib.*\s(\d+\s?±\s?\d+\s?[μ|n]m)|n?a?n?o?fibers?\swith\san?\s?a?v?e?r?a?g?e?\sdiameters?\s.*[\s≈~](\d+?\.?\d+[\s| ]?[μ|n]m)|diameters?\sof\sn?a?n?o?fibers?\s.*\s(\d+?\.?\d+[\s| ]?[μ|n]m)|fibers?\sdiameters?.*\s(\d+?\.?\d+\s?[μ|n]?m?\s\w+\s\d+?\.?\d+\s?[μ|n]?m?)|fibers?\sdiameters?.*[\s(](\d+[–|-]\d+\s?[μ|n]m)'
 
-test1 = 'to form fibres as 400 ± 100 nm in diameter at 4000 r min−1.'
-dia = re.findall(dia_rex, document)
-dia1 = re.findall(dia_rex, test1)
-
+#        dia = [(x.replace(',', '').replace(' ', '').replace(' ', '').replace(' ', '') if isinstance(x, str) else x for x in _ if x) for _ in dia]
+        
+#        dia = [(re.split('and|to|±|–|-', x) for x in _ if x) for _ in dia]
+#        dia = [(tuple(int(x) if x.isdigit() else x for x in _ if x)) for _ in dia]
+        test = test.append(dia)
 # =============================================================================
-# # Keep only a range
-# # clean tuple list of dia 
-# # (remove empty string → false min, space, comma and untis → conversion)
-# dia = [(x.replace(',', '').replace(' ', '') if isinstance(x, str) else x for x in _ if x) for _ in dia]
-# dia = [(tuple(int(x) if x.isdigit() else x for x in _ if x)) for _ in dia]
-# # find minimum dia
-# min0 = 300000000
-# for match in range(len(dia)):
-#     if isinstance(dia[match], int):
-#         min_dmatch = dia[match]
-#     else:
-#         min_dmatch = min(dia[match])
-#     if min_dmatch < min0:
-#         min0 = min_dmatch
-#         min_dia = min_dmatch
-# # find maximum dia        
-# max0 = 0
-# for match in range(len(dia)):
-#     if isinstance(dia[match], int):
-#         max_dmatch = dia[match]
-#     else:
-#         max_dmatch = max(dia[match])
-#     if  max0 < max_dmatch:
-#         max0 = max_dmatch
-#         max_dia = max_dmatch
-# # if no match is found
-# if not dia:
-#     min_dia = np.nan
-#     max_dia = np.nan
-#     min_dmatch = None
-#     max_dmatch = None
-# # put min max dia for a single article in a dataframe
-# dia_range1 = [min_dia, max_dia]
-# dia_range1 = pd.DataFrame(dia_range1, index=('min', 'max')).transpose()
-# # put min max dia for each article in a dataframe
-# dia_range = dia_range.append(dia_range1)
-# # clean before the new iteration of the loop
-# del min_dmatch, min_dia, max_dmatch, max_dia
+#         # find minimum dia
+#         min0 = 300000000
+#         for match in range(len(dia)):
+#             if isinstance(dia[match], int):
+#                 min_dmatch = dia[match]
+#             else:
+#                 min_dmatch = min(dia[match])
+#             if min_dmatch < min0:
+#                 min0 = min_dmatch
+#                 min_dia = min_dmatch
+#         # find maximum dia        
+#         max0 = 0
+#         for match in range(len(dia)):
+#             if isinstance(dia[match], int):
+#                 max_dmatch = dia[match]
+#             else:
+#                 max_dmatch = max(dia[match])
+#             if  max0 < max_dmatch:
+#                 max0 = max_dmatch
+#                 max_dia = max_dmatch
+#         # if no match is found
+#         if not dia:
+#             min_dia = np.nan
+#             max_dia = np.nan
+#             min_dmatch = None
+#             max_dmatch = None
+#         # put min max dia for a single article in a dataframe
+#         dia_range1 = [min_dia, max_dia]
+#         dia_range1 = pd.DataFrame(dia_range1, index=('min', 'max')).transpose()
+#         # put min max dia for each article in a dataframe
+#         dia_range = dia_range.append(dia_range1)
+#         # clean before the new iteration of the loop
+#         del min_dmatch, min_dia, max_dmatch, max_dia
+# 
 # =============================================================================
