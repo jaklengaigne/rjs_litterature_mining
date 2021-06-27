5c65de3df572400e2c5f7512395ceadb7de19eac
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-27 08:40:46 -0400

    Regex change for 012.html (fiber diameter range)
    test1 :+1:  ± works

--- DataMining/fiber_diameter_range.py
+++ DataMining/fiber_diameter_range.py
@@ -14,31 +14,18 @@ import numpy as np
 # =============================================================================
 dia_range = pd.DataFrame(columns=('min', 'max'))
 # Get article text from a saved html file
-path = './ArticlesHtml/024.html'
+path = './ArticlesHtml/012.html'
 f = codecs.open(path, 'r', 'utf-8')
 document = BeautifulSoup(f.read(),features='html.parser').get_text()
 f.close()
 
 # Get diameter
-dia_rex = r'n?a?n?o?fibers?\swith\san?\s?a?v?e?r?a?g?e?\sdiameters?\s.*[\s≈~](\d+?\.?\d+[\s| ]?[μ|n]m)|diameters?\sof\sn?a?n?o?fibers?\s.*\s(\d+?\.?\d+[\s| ]?[μ|n]m)|fibers?\sdiameters?.*\s(\d+?\.?\d+\s?[μ|n]?m?\s\w+\s\d+?\.?\d+\s?[μ|n]?m?)|fibers?\sdiameters?.*[\s(](\d+[–|-]\d+\s?[μ|n]m)'
+dia_rex = r'fib.*\s(\d+\s?±\s?\d+\s?[μ|n]m)|n?a?n?o?fibers?\swith\san?\s?a?v?e?r?a?g?e?\sdiameters?\s.*[\s≈~](\d+?\.?\d+[\s| ]?[μ|n]m)|diameters?\sof\sn?a?n?o?fibers?\s.*\s(\d+?\.?\d+[\s| ]?[μ|n]m)|fibers?\sdiameters?.*\s(\d+?\.?\d+\s?[μ|n]?m?\s\w+\s\d+?\.?\d+\s?[μ|n]?m?)|fibers?\sdiameters?.*[\s(](\d+[–|-]\d+\s?[μ|n]m)'
 
-test1 = 'polypropylene (PP) nanofibers with an average diameter of 790 nm are successfully prepared'
-test2 = 'Fang et al.11 prepared tin fluorophosphate glass fibers with average diameters ranging from 2 to 4 μm via centrifugal forcespinning.'
-test3 = 'the diameter of the centrifugal differential disk is 200 mm, and'
-test4 = 'the fibers were twisted into a cylinder about 3 mm in diameter and 20 mm in length, clamped on the nanotensile testing system (Aglient T150 UTM), the load value at start was 750 μN and'
-test5 = 'Table 2.The Influence of Four Studied Factors on the Average Diameter of Fibers Experimental number A B C D Average diameter (μm) 1 1 1 1 1 6.91'
-test6 = 'This is because the average diameter of fibers in Group 7 (0.79 μm) is much smaller than that in Group 2 (7.44 μm)'
-test7 = 'The maximum average diameter of fibers was 8.09 μm, '
-test8 = 'diameter of nanofibers was 160 nm for Group 7, as it is shown in Figure 7. It could indicate that the MDCE could fabricate the nanosized ultrafine fibers with a diameter of ~100 nm'
+test1 = 'to form fibres as 400 ± 100 nm in diameter at 4000 r min−1.'
 dia = re.findall(dia_rex, document)
 dia1 = re.findall(dia_rex, test1)
-dia2 = re.findall(dia_rex, test2)
-dia3 = re.findall(dia_rex, test3)
-dia4 = re.findall(dia_rex, test4)
-dia5 = re.findall(dia_rex, test5)
-dia6 = re.findall(dia_rex, test6)
-dia7 = re.findall(dia_rex, test7)
-dia8 = re.findall(dia_rex, test8)
+
 # =============================================================================
 # # Keep only a range
 # # clean tuple list of dia 
