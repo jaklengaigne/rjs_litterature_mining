055f194c8fac84c640e40486472e8f94605611a2
Valirou <68293328+Valirou@users.noreply.github.com>
2021-06-29 17:04:42 -0400

    Added min max df to fiber_diameter_range
    dia_range1 :+1:
    dia_range :-1:
    ... I do not know why min-max by article do not append as in speed_range!!!

--- DataMining/fiber_diameter_range.py
+++ DataMining/fiber_diameter_range.py
@@ -8,7 +8,6 @@ import numpy as np
 # Iterated throught a bunch of local html file
 directory = './ArticlesHtml'
 dia_range = pd.DataFrame(columns=('min', 'max'))
-test = pd.DataFrame()
 for filename in os.listdir(directory):
     if filename.endswith('.html'):
         dia_range = pd.DataFrame(columns=('min', 'max'))
@@ -77,54 +76,12 @@ for filename in os.listdir(directory):
                         dia.at[imatch, jgroup] = ele*10**-9
                 if isinstance(ele, str):
                     dia.at[imatch, jgroup] = np.nan
-        # Find min max
-        
-               
-                    
-                    
-        
-#        dia = [(re.split('[\s| ]±[\s| ]', x) if re.search('±', x) else x for x in _ if x) for _ in dia]
-#        dia = [re.split('[\s| ]', x[1]) if isinstance(x, list) else x for x in dia if x]
+        # find min max
+        min_dia = dia.min().min()
+        max_dia = dia.max().max()
+        # put min max dia for a single article in a dataframe
+        dia_range1 = [min_dia, max_dia]
+        dia_range1 = pd.DataFrame(dia_range1, index=('min', 'max')).transpose()
+        # put min max dia for each article in a dataframe
+        dia_range = dia_range.append(dia_range1)
 
-
-#        dia = [(x.replace(',', '').replace(' ', '').replace(' ', '').replace(' ', '') if isinstance(x, str) else x for x in _ if x) for _ in dia]
-        
-#        dia = [(re.split('and|to|±|–|-', x) for x in _ if x) for _ in dia]
-#        dia = [(tuple(int(x) if x.isdigit() else x for x in _ if x)) for _ in dia]
-        test = test.append(dia)
-# =============================================================================
-#         # find minimum dia
-#         min0 = 300000000
-#         for match in range(len(dia)):
-#             if isinstance(dia[match], int):
-#                 min_dmatch = dia[match]
-#             else:
-#                 min_dmatch = min(dia[match])
-#             if min_dmatch < min0:
-#                 min0 = min_dmatch
-#                 min_dia = min_dmatch
-#         # find maximum dia        
-#         max0 = 0
-#         for match in range(len(dia)):
-#             if isinstance(dia[match], int):
-#                 max_dmatch = dia[match]
-#             else:
-#                 max_dmatch = max(dia[match])
-#             if  max0 < max_dmatch:
-#                 max0 = max_dmatch
-#                 max_dia = max_dmatch
-#         # if no match is found
-#         if not dia:
-#             min_dia = np.nan
-#             max_dia = np.nan
-#             min_dmatch = None
-#             max_dmatch = None
-#         # put min max dia for a single article in a dataframe
-#         dia_range1 = [min_dia, max_dia]
-#         dia_range1 = pd.DataFrame(dia_range1, index=('min', 'max')).transpose()
-#         # put min max dia for each article in a dataframe
-#         dia_range = dia_range.append(dia_range1)
-#         # clean before the new iteration of the loop
-#         del min_dmatch, min_dia, max_dmatch, max_dia
-# 
-# =============================================================================
