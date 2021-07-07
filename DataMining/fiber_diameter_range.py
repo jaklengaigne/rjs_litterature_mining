9b7c88c9dec4d09fd45e0f08e9270ef32de91127
Valirou <68293328+Valirou@users.noreply.github.com>
2021-07-07 09:57:50 -0400

    Corrected fiber_diameter_range for 194 articles
    Just added → if not dia.empty:
    as in nozzle_diameter_range

--- DataMining/fiber_diameter_range.py
+++ DataMining/fiber_diameter_range.py
@@ -26,55 +26,56 @@ for filename in os.listdir(directory):
         dia = [(re.split(' and | to |–|-| | | ', x) if isinstance(x, str) else x for x in _ if x) for _ in dia]
         # ((remove generator because it's painfull to work with as beginner in python))
         dia = pd.DataFrame(dia)
-        dia = pd.DataFrame(dia[0].tolist())
-        # ((change object type because everything is not suppose to be str))
-        def isfloat(value):
-          try:
-            float(value)
-            return True
-          except ValueError:
-            return False
-        for i, row in dia.iterrows():
-            for j in range(len(row)):
-                if isinstance(row[j], str):
-                    if row[j].isdigit() or isfloat(row[j]):
-                        row[j] = float(row[j])
-                if not row[j]:
-                    row[j] = np.nan
-        # ((special case : ±))
-        bool_df = (dia == '±').any(axis=1)         
-        for imatch in range(len(dia)):
-            if bool_df[imatch]:
-                plus = dia.at[imatch, 0] + dia.at[imatch, 2]
-                new_rowp = [plus, dia.at[imatch, 3]]
-                new_rowp = pd.DataFrame(new_rowp).transpose()
-                
-                minus = dia.at[imatch, 0] - dia.at[imatch, 2]
-                new_rowm = [minus, dia.at[imatch, 3]]
-                new_rowm = pd.DataFrame(new_rowm).transpose()
-                
-                dia = pd.concat([dia, new_rowp])
-                dia = pd.concat([dia, new_rowm])
-                dia = dia.reset_index().drop('index', axis=1)  
-                dia = dia.drop(dia.index[imatch], axis=0)
-                dia = dia.reset_index().drop('index', axis=1)  
-        # ((conversion to meter))
-        for imatch in range(len(dia)):
-            for jgroup in range(len(dia.columns)):
-                ele = dia.at[imatch, jgroup]
-                if jgroup < len(dia.columns)-1:
-                    next_ele = dia.at[imatch, jgroup+1]
-                if isfloat(ele) and np.isfinite(ele):
-                    if isfloat(next_ele):
-                        unit = dia.at[imatch, jgroup+2]
-                    if isinstance(next_ele, str):
-                        unit = next_ele
-                    if unit == 'μm':
-                        dia.at[imatch, jgroup] = ele*10**-6
-                    if unit == 'nm':
-                        dia.at[imatch, jgroup] = ele*10**-9
-                if isinstance(ele, str):
-                    dia.at[imatch, jgroup] = np.nan
+        if not dia.empty:
+            dia = pd.DataFrame(dia[0].tolist())
+            # ((change object type because everything is not suppose to be str))
+            def isfloat(value):
+              try:
+                float(value)
+                return True
+              except ValueError:
+                return False
+            for i, row in dia.iterrows():
+                for j in range(len(row)):
+                    if isinstance(row[j], str):
+                        if row[j].isdigit() or isfloat(row[j]):
+                            row[j] = float(row[j])
+                    if not row[j]:
+                        row[j] = np.nan
+            # ((special case : ±))
+            bool_df = (dia == '±').any(axis=1)         
+            for imatch in range(len(dia)):
+                if bool_df[imatch]:
+                    plus = dia.at[imatch, 0] + dia.at[imatch, 2]
+                    new_rowp = [plus, dia.at[imatch, 3]]
+                    new_rowp = pd.DataFrame(new_rowp).transpose()
+                    
+                    minus = dia.at[imatch, 0] - dia.at[imatch, 2]
+                    new_rowm = [minus, dia.at[imatch, 3]]
+                    new_rowm = pd.DataFrame(new_rowm).transpose()
+                    
+                    dia = pd.concat([dia, new_rowp])
+                    dia = pd.concat([dia, new_rowm])
+                    dia = dia.reset_index().drop('index', axis=1)  
+                    dia = dia.drop(dia.index[imatch], axis=0)
+                    dia = dia.reset_index().drop('index', axis=1)  
+            # ((conversion to meter))
+            for imatch in range(len(dia)):
+                for jgroup in range(len(dia.columns)):
+                    ele = dia.at[imatch, jgroup]
+                    if jgroup < len(dia.columns)-1:
+                        next_ele = dia.at[imatch, jgroup+1]
+                    if isfloat(ele) and np.isfinite(ele):
+                        if isfloat(next_ele):
+                            unit = dia.at[imatch, jgroup+2]
+                        if isinstance(next_ele, str):
+                            unit = next_ele
+                        if unit == 'μm':
+                            dia.at[imatch, jgroup] = ele*10**-6
+                        if unit == 'nm':
+                            dia.at[imatch, jgroup] = ele*10**-9
+                    if isinstance(ele, str):
+                        dia.at[imatch, jgroup] = np.nan
         # find min max
         min_dia = dia.min().min()
         max_dia = dia.max().max()
